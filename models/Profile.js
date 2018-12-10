1b2f47cad3efd8512f40da5045619d67ffd940d4
mariuszdw <mariusz.dw@gmail.com>
2018-12-11 00:07:26 +0100

    correct typo

--- models/Profile.js
+++ models/Profile.js
@@ -4,94 +4,94 @@ const Schema = mongoose.Schema;
 // Create Schema
 
 const ProfileSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-    },
-    handle: {
-        type: String,
-        required: true,
-        max: 40
-    },
-    company: String,
-    website: String,
-    location: String,
-    status: {
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: String,
+  website: String,
+  location: String,
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: String,
+  githubusername: String,
+  experience: [
+    {
+      title: {
         type: String,
         required: true
-    },
-    skills: {
-        type: [String],
+      },
+      company: {
+        type: String,
         required: true
-    },
-    bio: String,
-    githubusername: String,
-    experience: [
-        {
-            title: {
-                type: String,
-                required: true
-            },
-            company: {
-                type: String,
-                required: true
-            },
-            location: {
-                type: String              
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: String
-        }
-    ],
-    education: [
-        {
-            scool: {
-                type: String,
-                required: true
-            },
-            degree: {
-                type: String,
-                required: true
-            },
-            fieldofstudy: {
-                type: String,
-                required: true              
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: String
-        }
-    ],linkedinlinkedin
-    social: {
-        youtube: String,
-        twitter: String,
-        facebook: String,
-        linkedin: String,
-        instagram: String,
-    },
-    date: {
+      },
+      location: {
+        type: String
+      },
+      from: {
         type: Date,
-        default: Date.now
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: String
     }
-})
+  ],
+  education: [
+    {
+      scool: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: String
+    }
+  ],
+  social: {
+    youtube: String,
+    twitter: String,
+    facebook: String,
+    linkedin: String,
+    instagram: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-module.exports = Profile = mongoose.model('profiles', ProfileSchema);
+module.exports = Profile = mongoose.model("profiles", ProfileSchema);
